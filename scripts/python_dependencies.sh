ec56058601362017ba995bced27c8799c10648a9
Sonali824 <sonalimaharanafeb@gmail.com>
2024-09-08 21:38:00 +0530

    Update python_dependencies.sh

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
 sudo chown -R ubuntu:ubuntu ~/new-node
-sudo apt install python3.9 python3.9-venv
-python3.9 -m venv /home/ubuntu/new-node/venv
-if [ ! -d "/home/ubuntu/new-node/venv" ]; then
-    virtualenv /home/ubuntu/new-node/venv
-fi
+virtualenv /home/ubuntu/new-node/venv
 source /home/ubuntu/new-node/venv/bin/activate
 pip install -r /home/ubuntu/new-node/requirements.txt
