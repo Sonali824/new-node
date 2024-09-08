f9e84e8340f9fb8ae539cc40fbd23bd673a2051a
Sonali824 <sonalimaharanafeb@gmail.com>
2024-09-08 21:19:38 +0530

    Update python_dependencies.sh

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 sudo chown -R ubuntu:ubuntu ~/new-node
-virtualenv /home/ubuntu/new-node/venv
+if [ ! -d "/home/ubuntu/new-node/venv" ]; then
+    virtualenv /home/ubuntu/new-node/venv
+fi
 source /home/ubuntu/new-node/venv/bin/activate
 pip install -r /home/ubuntu/new-node/requirements.txt
